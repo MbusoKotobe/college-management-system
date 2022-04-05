2845dffd407dd974b4b7bd91c05b210e4638a7cf
mbusokotobewrk@gmail.com <mbusokotobewrk@gmail.com>
2022-04-05 20:00:50 +0200

    Initial Commit

---
+++ src/test/java/za/ac/cput/entity/DepartmentTest.java
@@ -0,0 +1,13 @@
+package za.ac.cput.entity;
+
+import org.junit.jupiter.api.AfterEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DepartmentTest
+{
+    @AfterEach
+    void tearDown ()
+    {
+    }
+}
---
+++ src/test/java/za/ac/cput/repository/DepartmentRepositoryTest.java
@@ -0,0 +1,8 @@
+package za.ac.cput.repository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DepartmentRepositoryTest
+{
+
+}
