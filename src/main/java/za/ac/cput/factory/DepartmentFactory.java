7c18c002911dd3cba4df6a5613d2c3dd4458c363
mbusokotobewrk@gmail.com <mbuso.kotobes@gmail.com>
2022-04-06 16:25:11 +0200

    TASK: Implemented Department Factory.

--- src/main/java/za/ac/cput/factory/DepartmentFactory.java
+++ src/main/java/za/ac/cput/factory/DepartmentFactory.java
@@ -1,5 +1,13 @@
 package za.ac.cput.factory;
 
+import za.ac.cput.entity.Department;
+
 public class DepartmentFactory
 {
+    public static Department createDepartment(String departmentName, String departmentDescription)
+    {
+        return new Department.Builder().setDepartmentName(departmentName)
+                                       .setDepartmentDescription(departmentDescription)
+                                       .build();
+    }
 }
