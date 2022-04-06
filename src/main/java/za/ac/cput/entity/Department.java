6542a029f8370297924f0dc10344d81ee2b13816
mbusokotobewrk@gmail.com <mbuso.kotobes@gmail.com>
2022-04-06 10:14:27 +0200

    TASK: Added Faculty entity.

--- src/main/java/za/ac/cput/entity/Department.java
+++ src/main/java/za/ac/cput/entity/Department.java
@@ -1,6 +1,77 @@
 package za.ac.cput.entity;
 
-public class Department
-{
+/***
+ * Department.java
+ * Entity for the Department
+ * @Author: Mbuso Kotobe (218040385)
+ * Date: 06 April 2022
+ */
+public class Department {
+    int deparmentId;
+    String departmentName;
+    String departmentDescription;
+    Faculty faculty;
 
+    private Department(Builder department)
+    {
+        this.deparmentId = department.deparmentId;
+        this.departmentName = department.departmentName;
+        this.departmentDescription = department.departmentDescription;
+    }
+
+    public int getDeparmentId ()
+    {
+        return deparmentId;
+    }
+
+    public String getDepartmentName ()
+    {
+        return departmentName;
+    }
+
+    public String getDepartmentDescription ()
+    {
+        return departmentDescription;
+    }
+
+    public Faculty getFaculty ()
+    {
+        return faculty;
+    }
+
+    class Builder{
+        int deparmentId;
+        String departmentName;
+        String departmentDescription;
+        Faculty faculty;
+
+        public Builder setDeparmentId (int deparmentId)
+        {
+            this.deparmentId = deparmentId;
+            return this;
+        }
+
+        public Builder setDepartmentName (String departmentName)
+        {
+            this.departmentName = departmentName;
+            return this;
+        }
+
+        public Builder setDepartmentDescription (String departmentDescription)
+        {
+            this.departmentDescription = departmentDescription;
+            return this;
+        }
+
+        public Builder setFaculty (Faculty faculty)
+        {
+            this.faculty = faculty;
+            return this;
+        }
+
+        public Department build()
+        {
+            return new Department(this);
+        }
+    }
 }
--- src/main/java/za/ac/cput/entity/Faculty.java
+++ src/main/java/za/ac/cput/entity/Faculty.java
@@ -1,6 +1,29 @@
 package za.ac.cput.entity;
 
+/***
+ * Faculty.java
+ * Entity for the Faculty
+ * @Author: Mbuso Kotobe (218040385)
+ * Date: 06 April 2022
+ */
 public class Faculty
 {
+    int facultyId;
+    String facultyName;
+    String facultyDescription;
 
+    public int getFacultyId ()
+    {
+        return facultyId;
+    }
+
+    public String getFacultyName ()
+    {
+        return facultyName;
+    }
+
+    public String getFacultyDescription ()
+    {
+        return facultyDescription;
+    }
 }
