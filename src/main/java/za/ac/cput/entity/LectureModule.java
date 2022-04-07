18769f6316423f8f37e52ee5a0c01328fc79ac82
Elvis <213063964@mycput.ac.za>
2022-04-07 17:48:16 +0200

    TASK: Added LectureModule.java entity

--- src/main/java/za/ac/cput/entity/LectureModule.java
+++ src/main/java/za/ac/cput/entity/LectureModule.java
@@ -1,4 +1,35 @@
+/***
+ * Department.java
+ * Entity for the Department
+ * @Author: Elvis Ndlangamandla (213063964)
+ * Date: 06 April 2022
+ */
 package za.ac.cput.entity;
 
 public class LectureModule {
+    private long lectureId;
+    private String moduleId;
+
+    private LectureModule (StudentDepartment.Builder builder) {
+        this.lectureId = lectureId;
+        this.moduleId = moduleId;
+    }
+
+    public long getLectureId() {
+        return lectureId;
+    }
+
+    public void setLectureId(long lectureId) {
+        this.lectureId = lectureId;
+    }
+
+    public String getModuleId() {
+        return moduleId;
+    }
+
+    public void setModuleId(String moduleId) {
+        this.moduleId = moduleId;
+    }
+
+
 }
