cbc17506f1df42ca7b8c5b92e28f36817cc42d7a
anelealtose <216079292@mycput.ac.za>
2022-04-09 15:08:19 +0200

    Added the test classes for: LecturerFactory, LecturerDepartmentFactory.

---
+++ src/test/java/za/ac/cput/factory/lecturerFactoryTest.java
@@ -0,0 +1,23 @@
+package za.ac.cput.factory;
+
+import org.junit.jupiter.api.Test;
+import za.ac.cput.entity.Lecturer;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class lecturerFactoryTest {
+    @Test
+    public void add(){
+        Lecturer l = new Lecturer.Builder().setLectureId(12252258).setFirstName("Arinze")
+                .setLastName("Anikue").build();
+        assertNotNull(l);
+    }
+    @Test
+    public void copy(){
+        Lecturer l = new Lecturer.Builder().setLectureId(12252258).setFirstName("Arinze")
+                .setLastName("Anikue").build();
+        Lecturer l2 = new Lecturer.Builder().copy(l).build();
+        assertEquals(l.getFirstName(), l2.getFirstName());
+    }
+
+}
